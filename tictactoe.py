adb18b6c4c761f8ec95d5c4e1f2555fe6bbc3511
alexsouthwick18 <91078301+alexsouthwick18@users.noreply.github.com>
2022-11-15 09:37:32 -0700

    player turns

--- tictactoe.py
+++ tictactoe.py
@@ -1,13 +1,19 @@
+# ----- Alex Southwick -----
+
+
+# ----- Main function -----
 def main():
     board = make_board()
     show_board(board)
 
+# ----- Function for creating the board boxes -----
 def make_board():
     board = []
     for box in range(9):
         board.append(box +1)
     return board
 
+# ----- Function for showing the board to the player -----
 def show_board(board):
     print()
     print(f"{board[0]}|{board[1]}|{board[2]}")
@@ -17,5 +23,18 @@ def show_board(board):
     print(f"{board[6]}|{board[7]}|{board[8]}")
     print()
 
+# ----- Function for alternating player turns each round -----
+def player_turn(player, board):
+    box = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[box - 1] = player
+
+# ----- Function for finding out who's turn it is (If/Then block) -----
+def player_current(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+# ----- Run that main function -----
 if __name__ == "__main__":
     main()
